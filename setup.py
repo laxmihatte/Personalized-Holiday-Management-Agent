cfe9f314180258bc8367e349091d41f75675481d
Laxmi Hatte <laxmihatte234@gmail.com>
2026-06-17 19:31:44 -0400

    setup file added

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='holiday_management',
     version='0.1.0',
-    author="Boktiar Ahmed Bappy",
+    author="Laxmi",
+    author_email="laxmihatte234@gmail.com",
     packages=find_packages()
 )
